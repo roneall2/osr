0ed73d5507582bbf83f5357058cbb60a9cdd1b33
BasselAbuJabal <60801062+BasselAbuJabal@users.noreply.github.com>
2020-11-30 21:35:39 -0600

    Add files via upload

--- OCR.py
+++ OCR.py
@@ -3,16 +3,18 @@ import re
 from collections import defaultdict
 
 import pytesseract
+
 from PIL import Image
 import cv2
 from cv2 import cv2
 import numpy as np
+from collections import defaultdict
 
 #open cv accepts BGR, while pytesseract accepts RBG
 from pytesseract import Output
 
 def image2Text():
-#file_path
+    #file_path
     pytesseract.pytesseract.tesseract_cmd= r"C:\Program Files\Tesseract-OCR\tesseract.exe"
     config = r"--oem 3 --psm 6"
     print('hello world')
@@ -25,8 +27,9 @@ def image2Text():
     print(recieptTxt)
     print(splitText)
 
-    product_catalog={}
+    product_catalog=defaultdict(int)
     product_catalog=image2Data(splitText,product_catalog)
+    outputToSql(product_catalog)
     print(product_catalog)
     printImage(image)
 
@@ -53,6 +56,7 @@ def directory2Text():
             print(splitText)
 
             product_catalog = image2Data(splitText,product_catalog)
+            outputToSql(product_catalog)
             print(product_catalog)
             #printImage(image)
         else:
@@ -86,44 +90,6 @@ def preProcess(image):
 
 
 
-#
-# def process_image_for_ocr(file_path):
-#     # TODO : Implement using opencv
-#     temp_filename = set_image_dpi(file_path)
-#     im_new = remove_noise_and_smooth(temp_filename)
-#     return im_new
-#
-# def set_image_dpi(file_path):
-#     im = Image.open(file_path)
-#     length_x, width_y = im.size
-#     factor = max(1, int(IMAGE_SIZE / length_x))
-#     size = factor * length_x, factor * width_y
-#     # size = (1800, 1800)
-#     im_resized = im.resize(size, Image.ANTIALIAS)
-#     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.jpg')
-#     temp_filename = temp_file.name
-#     im_resized.save(temp_filename, dpi=(300, 300))
-#     return temp_filename
-#
-# def image_smoothening(img):
-#     ret1, th1 = cv2.threshold(img, BINARY_THRESHOLD, 255, cv2.THRESH_BINARY)
-#     ret2, th2 = cv2.threshold(th1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-#     blur = cv2.GaussianBlur(th2, (1, 1), 0)
-#     ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-#     return th3
-#
-# def remove_noise_and_smooth(file_name):
-#     img = cv2.imread(file_name, 0)
-#     filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 41,
-#                                      3)
-#     kernel = np.ones((1, 1), np.uint8)
-#     opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
-#     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-#     img = image_smoothening(img)
-#     or_image = cv2.bitwise_or(img, closing)
-#     return or_image
-
-
 def image2Data(splitText, product_catalog):
     #Item: Brand, Name, Price
     ##Read and Output text
@@ -137,40 +103,49 @@ def image2Data(splitText, product_catalog):
     product_pattern=re.compile(r'([A-Z\s_.]+)[\d\s]+..?\s?\${0,1}(\d+\.\d{2})')
     total_pattern=re.compile(r'(SUBTOTAL|TOTAL)([/s0-9])*')
     store_pattern=re.compile(r'(WALMAR|TARGET|COSTCO|SAM\'S)')
+    address_pattern = re.compile(r'/d{4}\s/w+')
 
     #productPattern=r'\s(\d+)\s(\d+\.\d{2})'
     productPattern=re.compile(r'\d*\.\d\d')
 
     #([A-Z0-9.\s])
     for line in splitText:
-      if re.search(date_pattern,line):
-          items.append(line)
-          product_line = productPattern.search(line)
-          print("first loop",product_line)
-      # elif re.match(price_pattern, line):
-      #    date= line
-      elif re.search(product_pattern, line):
+        if re.search(date_pattern,line):
+              items.append(line)
+              product_line = productPattern.search(line)
+              print("first loop",product_line)
+          # elif re.match(price_pattern, line):
+          #    date= line
+        elif re.search(product_pattern, line):
           product_line=product_pattern.search(line)
           print("second loop",product_line.group(1))
           products.append(product_line.group(1))
           prices.append(product_line.group(2))
           product_catalog[product_line.group(1)]=product_line.group(2)
-      elif re.search(total_pattern, line):
+        elif re.search(total_pattern, line):
           print("subtotal and total",line)
           total_line = total_pattern.search(line)
+          product_catalog[total_line.group(1)]=0
           product_catalog[total_line.group(1)]=total_line.group(2)
-      elif re.search(store_pattern, line):
-          print("store name", line)
-          store_line = store_pattern.search(line)
-          product_catalog["store_name"] = store_line.group(1)
-
+        elif re.search(store_pattern, line):
+            print("store name", line)
+            store_line = store_pattern.search(line)
+            product_catalog["store_name"] = store_line.group(1)
+        elif re.search(address_pattern, line):
+            print("store name", line)
+            address_line = address_pattern.search(line)
+            product_catalog["store_location"] = address_line
     print("date:", items)
     print("product names:", products)
     print("prices:", prices)
-    #print(product_catalog)
     return product_catalog
-    #print(items)
 
+def outputToSql(product_catalog):
+    store_id = check_store(product_catalog["store_name"], product_catalog["location"], "")
+    purchase_id=add_receipt(0, store_id, product_catalog["SUBTOTAL"], product_catalog["TOTAL"])
+    for name in product_catalog:
+        price = product_catalog[name]
+        add_item(purchase_id, "brand name", name, price)
 
 def printImage(image):
     d = pytesseract.image_to_data(image, output_type=Output.DICT)
@@ -189,6 +164,7 @@ def printImage(image):
     cv2.imshow('img', image)
     cv2.waitKey(0)
 
+
     #get boxes
     n_boxes = len(d['text'])
     for i in range(n_boxes):
@@ -203,10 +179,7 @@ def printImage(image):
 
 
 if __name__ == '__main__':
-    #image2Text()
-    directory2Text()
-
-
-
+    image2Text()
+    #directory2Text()
 
 
